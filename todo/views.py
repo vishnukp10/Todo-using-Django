f9155f8fc82f058a902d141bdb7913f3a62f841e
vishnukp10 <49436839+vishnukp10@users.noreply.github.com>
2022-04-02 14:40:15 +0530

    Add files via upload

---
+++ todo/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from .models import *
+# Create your views here.
+def index(request):
+    todo =Todo.objects.all()
+    if request.method == 'POST':
+        new_todo = Todo(
+            title = request.POST['title']
+        )
+        new_todo.save()
+        return redirect('/')
+    return render(request, 'index.html', {'todos':todo})
+
+def delete(request, pk):
+    todo = Todo.objects.get(id=pk)
+    todo.delete()
+    return redirect('/')
